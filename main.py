51e85762d945b8236cc6cc6ee58660079c54c24c
HarshKumar0905 <pranshu09051@gmail.com>
2026-01-13 16:35:56 +0530

    Foundation agent with AgentExecutor and React Prompt

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
 
 load_dotenv()
-client = Client()
-tools = [TavilyClient()]
 
+tools = [TavilySearch()]
+react_prompt = hub.pull("hwchase17/react")
+# Upgraded to Gemini 3 Flash for improved performance and reasoning
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
-prompt = client.pull_prompt("hwchase17/react")
-agent = create_agent(model=llm, tools=tools)
+agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
 
 def main():
-    result = agent.invoke({"messages": HumanMessage(content="Search for 3 job postings for an ai engineer in New York in linkedin and list their details")})
-
+    result = chain.invoke(
+        input={
+            "input": "Fetch me the latest 3 job openings for AI Engineer from linkedin in the NYC region."
+        }
+    )
+    print(result)
 
 if __name__ == "__main__":
     main()
