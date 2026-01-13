3df3c5157e942ba88571586ce65c3356bb272594
HarshKumar0905 <pranshu09051@gmail.com>
2026-01-13 15:38:22 +0530

    Initial project setup

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from tavily import TavilyClient
+
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+
+load_dotenv()
+client = Client()
+tools = [TavilyClient()]
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+prompt = client.pull_prompt("hwchase17/react")
+agent = create_agent(model=llm, tools=tools)
+
 def main():
-    print("Hello from langchain-course!")
+    result = agent.invoke({"messages": HumanMessage(content="Search for 3 job postings for an ai engineer in New York in linkedin and list their details")})
 
 
 if __name__ == "__main__":
